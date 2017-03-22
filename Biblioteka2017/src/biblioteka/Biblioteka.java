5f99793dd78f68d6c5f1ab363f2b134a33a2047f
ivan996 <ivan.stanimirovic692@gmail.com>
2017-03-22 14:49:52 +0100

    Uvedena logicka kontrola

--- Biblioteka2017/src/biblioteka/Biblioteka.java
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -9,11 +9,15 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public void dodajKnigu(Knjiga k) {
+		if (k == null)
+			throw new RuntimeException();
 		knjige.add(k);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
+		if (k == null)
+			throw new RuntimeException();
 		knjige.remove(k);
 	}
 
@@ -24,6 +28,9 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac) {
+		if (autor == null || isbn == 0 || naslov == null || izdavac == null)
+			throw new RuntimeException("Morate uneti neki kriterujm");
+
 		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
 
 		for (int i = 0; i < knjige.size(); i++) {
